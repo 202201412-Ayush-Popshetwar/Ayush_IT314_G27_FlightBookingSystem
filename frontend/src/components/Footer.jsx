88594b29ac28fff22a98a9f89ef042b5742eff9c
Anonymous <swapnilshukla470@gmail.com>
2024-11-18 01:27:36 +0530

    Linking About us page in the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -6,16 +7,16 @@ const Footer = () => {
             <div className="max-w-[1024px] mx-auto px-[5vw]">
                 <div className="flex flex-wrap justify-between items mb-[4vh]">
                     <div className="footer-section about w-full sm:w-1/3 mb-4">
-                        <h2 className="text-xl font-bold mb-2t">FlightBookingSystem</h2>
+                        <h2 className="text-xl font-bold mb-2">FlightBookingSystem</h2>
                         <p>Your one-stop solution for booking flights with ease and convenience.</p>
                     </div>
                     <div className="footer-section-links w-full sm:w-1/3 mb-4">
                         <h2 className="text-xl font-bold mb-2">Quick Links</h2>
                         <ul>
-                            <li><a href="/" className="text-white hover text-gray-100 hover:underline">Home</a></li>
-                            <li><a href="/about" className="text-white hover text-gray-100 hover:underline">About Us</a></li>
-                            <li><a href="/contact" className="text-white hover text-gray-100 hover:underline">Contact Us</a></li>
-                            <li><a href="/faq" className="text-white hover text-gray-100 hover:underline">FAQ</a></li>
+                            <li><Link to="/" className="text-white hover:text-gray-100 hover:underline">Home</Link></li>
+                            <li><Link to="/aboutus" className="text-white hover:text-gray-100 hover:underline">About Us</Link></li>
+                            <li><Link to="/contactus" className="text-white hover:text-gray-100 hover:underline">Contact Us</Link></li>
+                            <li><Link to="/faq" className="text-white hover:text-gray-100 hover:underline">FAQ</Link></li>
                         </ul>
                     </div>
                     <div className="footer-section-contact w-full sm:w-1/3 mb-4">
